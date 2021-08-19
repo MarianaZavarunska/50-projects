3184ee78e58024cfe9c424fb7c01a2ac8b3eccd5
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-08-19 21:54:36 +0300

    change variable name

--- verify-ac/script.js
+++ verify-ac/script.js
@@ -2,13 +2,13 @@ const codes = document.querySelectorAll('.code');
 
 codes[0].focus();
 
-codes.forEach((code, idx) => {
+codes.forEach((code, index) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <= 9) {
-            codes[idx].value = ''
-            setTimeout(() => codes[idx + 1].focus(), 10)
+            codes[index].value = ''
+            setTimeout(() => codes[index + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
+            setTimeout(() => codes[index - 1].focus(), 10)
         }
     })
 })
